b8f0acfab50f996cbd59eaf4d51036a4f495935b
Jacob Parker <jacobparker1992@gmail.com>
2019-02-12 20:36:59 +0000

    .

--- src/__tests__/q-util.js
+++ src/__tests__/q-util.js
@@ -2,25 +2,33 @@ const rat = require("big-rat");
 const cartesian = require("cartesian");
 const { magnitude } = require("../Math/QUtil.bs");
 
-it("calculates q magnitude", () => {
+describe("calculates q magnitude", () => {
   const values = [
-    1,
-    2,
-    5,
-    53,
-    208,
-    3918,
-    48303,
-    5842753,
-    95482053253,
-    9584205482539355423,
-    8549051290485403254302594230548325432043225
+    "1",
+    "2",
+    "5",
+    "10",
+    "53",
+    "100",
+    "208",
+    "1000",
+    "3918",
+    "48303",
+    "5842753",
+    "95482053253",
+    "9584205482539355423",
+    "8549051290485403254302594230548325432043225",
+    "100000000000000000000000000000000000000000000000000000000000000000"
   ];
 
   cartesian([[1, -1], values, values]).forEach(([mul, num, den]) => {
-    const q = rat(mul * num, den);
-    const mag = magnitude(q);
-    const jsMag = Number((mul * (num / den)).toExponential().split("e")[1]);
-    expect(mag).toBe(jsMag);
+    const sign = mul === 1 ? "" : "-";
+
+    it(`for ${sign}${num}/${den}`, () => {
+      const q = rat(sign + num, den);
+      const mag = magnitude(q);
+      const jsMag = +(mul * (+num / +den)).toExponential().split("e")[1];
+      expect(mag).toBe(jsMag);
+    });
   });
 });
